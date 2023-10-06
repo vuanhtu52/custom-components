99da0a0551dda488b4a8fabc3b22691ec99e9735
Tu Vu <vuanhtu52@gmail.com>
2023-10-06 16:09:37 +0700

    Go to next image when user clicks next

--- src/modules/imageSlider/imageSlider.js
+++ src/modules/imageSlider/imageSlider.js
@@ -32,38 +32,61 @@ const createImageSlider = ({imageLinks}) => {
     });
     slider.appendChild(dots);
 
-    // Add the first image to the container
-    const image = new Image();
-    image.src = imageLinks[0];
-    slider.querySelector(".container").appendChild(image);
-    slider.index  = 0;
+    // Add the images to the container
+    for (let i = 0; i < imageLinks.length; i++) {
+        const image = new Image();
+        image.src = imageLinks[i];
+        if (i !== 0) {
+            // image.style.display = "none";
+            image.style.visibility = "hidden";
+        }
+        slider.querySelector(".container").appendChild(image);
+        slider.imageIndex = 0;
+    }
+
+    // Detect when an image finishes the animation
+    slider.querySelectorAll(".container > img").forEach(image => {
+        image.addEventListener("animationend", () => {
+            console.log(image, image.style.animationName);
+            if (image.style.animationName === "slide-left-out") {
+                // image.style.display = "none";
+                image.style.visibility = "hidden";
+            } else if (image.style.animationName === "slide-left-in") {
+                // image.style.display = "block";
+                image.style.visibility = "visible";
+            }
+        });
+    });
 
     // Detect when user clicks the next button
     slider.querySelector(".next-button").addEventListener("click", () => {
-        console.log(imageLinks[0]);
-        // toNextSlide({slider: slider, imageLinks: imageLinks});
+        toNextSlide({slider: slider});
     });
+
 
     return slider;
 };
 
-// const toNextSlide = ({slider, imageLinks}) => {
-//     // Remove the current image from container
-//     slider.querySelector(".container").removeChild(slider.querySelector(".container").lastChild);
 
-//     // Calculate the index of the next image
-//     let newIndex = slider.index;
-//     if (newIndex === imageLinks.length - 1) {
-//         newIndex = 0;
-//     } else {
-//         newIndex += 1;
-//     }
-//     slider.index = newIndex;
+const toNextSlide = ({slider}) => {
+    // Hide the current image
+    const currentImage = slider.querySelector(".container").children.item(slider.imageIndex);
+    currentImage.style.animation = "1s slide-left-out";
 
-//     // Add new image
-//     const image = new Image();
-//     image.src = imageLinks[slider.index];
-//     slider.querySelector(".container").appendChild(image);
-// };
+
+    // Calculate the index of the next image
+    let newIndex = slider.imageIndex;
+    console.log(newIndex);
+    if (newIndex === slider.querySelector(".container").children.length - 1) {
+        newIndex = 0;
+    } else {
+        newIndex += 1;
+    }
+    slider.imageIndex = newIndex;
+
+    // Show the next image
+    const newImage = slider.querySelector(".container").children.item(slider.imageIndex);
+    newImage.style.animation = "1s slide-left-in"
+}
 
 export default createImageSlider;
